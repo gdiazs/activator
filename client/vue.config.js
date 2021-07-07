0a93b1e921c44c11465e1098d225cd69ca33104f
Guillermo <memo@pop-os.localdomain>
2021-07-06 22:04:15 -0600

    Update

---
+++ client/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    lintOnSave: false,
+    productionSourceMap: false,
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.optimization.splitChunks(false)
+    },
+    publicPath: "/",
+    devServer: {
+        proxy: {
+            "/*": {
+                target: 'http://localhost:5000/',
+                changeOrigin: true
+            }
+        },
+        disableHostCheck: true,
+        port: 9000
+    }
+}
